91e516f228306ef422090ae86c1deef802354a74
Raj Shekhar Singh <raj@gramfactory.com>
2020-07-21 02:39:32 +0530

    removed redundant model which was added twice earlier and then the apps were seperated

--- oyerickshaw/accounts_locations/models.py
+++ oyerickshaw/accounts_locations/models.py
@@ -65,14 +65,3 @@ class User(AbstractUser):
     def __str__(self):
         return "%s - %s"%(str(self.phone_number), self.first_name)
 
-
-class DriverRickshaw(models.Model):
-    user = models.ForeignKey(User, related_name='user_rickshaw_id', on_delete=models.CASCADE)
-    rickshaw_number = models.CharField(max_length=255, null = True)
-    status = models.BooleanField(default=False)
-
-    def save(self, *args, **kwargs):
-        if self.status == True:
-            DriverRickshaw.objects.filter(user = self.user, status=True).update(status=False)
-            self.status = True
-        super().save(*args, **kwargs)
